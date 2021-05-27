f46f707cc6cb12f5a35a9e5493b51c55acafa43f
MikeLrUC <rabuge@student.dei.uc.pt>
2021-05-27 19:54:31 +0100

    'db' folder refactor to new schema

--- src/db/data.sql
+++ src/db/data.sql
@@ -1,17 +1,22 @@
 -- User --
 
-INSERT INTO users (
-    person_username,
-    person_password, 
-    person_email
+INSERT INTO person (
+    username,
+    password, 
+    email
 )
 VALUES 
-(
+( -- id: 0
+    'admin',
+    'admin', 
+    'admin@admin.admin'
+),
+( -- id: 1
     'Marega',
     'Maregod', 
     'marega@god.kekw'
 ),
-(
+( -- id: 2
     'Pedro Mitalves',
     'PoteDouro',
     'seradesta@might.actually'
@@ -20,17 +25,13 @@ VALUES
 
 -- Administrator --
 
-INSERT INTO administrator (
-    person_username, 
-    person_password, 
-    person_email
-)
-VALUES 
-(
-    'admin',
-    'admin', 
-    'admin@admin.admin'
-);
+INSERT INTO administrator (person_id)
+VALUES (1);
+
+-- User --
+
+INSERT INTO users (person_id)
+VALUES (2), (3);
 
 
 -- Auction --
@@ -39,12 +40,12 @@ INSERT INTO auction (
     item,
     min_price,
     end_date,
-    administrator_person_id
+    person_id
 )
 VALUES 
 (12344, 32.3, TIMESTAMP '2021-06-12 04:05:06', 1),
-(420, 4.20, TIMESTAMP '2021-04-20 04:20:00', 1);
-(123, 123.4, TIMESTAMP '2021-01-23 01:23:45', 2),
+(420, 4.20, TIMESTAMP '2021-04-20 04:20:00', 1),
+(123, 123.4, TIMESTAMP '2021-01-23 01:23:45', 2);
 
 
 -- Information --
--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,75 +1,159 @@
+-- CREATE TABLE licitation (
+--     price FLOAT(8) NOT NULL,
+--     valid BOOL NOT NULL DEFAULT true,
+--     auction_id BIGINT,
+--     administrator_person_id BIGINT,
+--     PRIMARY KEY(auction_id, administrator_person_id)
+-- );
+-- CREATE TABLE auction (
+--     id BIGSERIAL,
+--     item BIGINT NOT NULL,
+--     min_price FLOAT(8) NOT NULL DEFAULT 0,
+--     end_date TIMESTAMP NOT NULL,
+--     cancelled BOOL NOT NULL DEFAULT false,
+--     administrator_person_id BIGINT NOT NULL,
+--     PRIMARY KEY(id)
+-- );
+-- CREATE TABLE users (
+--     person_id BIGSERIAL,
+--     person_username VARCHAR(512) NOT NULL UNIQUE,
+--     person_password VARCHAR(512) NOT NULL,
+--     person_email VARCHAR(512) NOT NULL,
+--     person_banned BOOL NOT NULL DEFAULT false,
+--     PRIMARY KEY(person_id)
+-- );
+-- CREATE TABLE administrator (
+--     person_id BIGSERIAL,
+--     person_username VARCHAR(512) NOT NULL,
+--     person_password VARCHAR(512) NOT NULL,
+--     person_email VARCHAR(512) NOT NULL,
+--     person_banned BOOL NOT NULL DEFAULT false,
+--     PRIMARY KEY(person_id)
+-- );
+-- CREATE TABLE message (
+--     id BIGSERIAL,
+--     content VARCHAR(1024) NOT NULL,
+--     time_date TIMESTAMP NOT NULL,
+--     administrator_person_id BIGINT NOT NULL,
+--     auction_id BIGINT NOT NULL,
+--     PRIMARY KEY(id)
+-- );
+-- CREATE TABLE information (
+--     reference BIGSERIAL,
+--     title VARCHAR(512) NOT NULL,
+--     description VARCHAR(1024) NOT NULL,
+--     auction_id BIGINT NOT NULL,
+--     PRIMARY KEY(reference)
+-- );
+-- CREATE TABLE notification (
+--     id BIGSERIAL,
+--     content VARCHAR(1024) NOT NULL,
+--     time_date TIMESTAMP NOT NULL,
+--     was_read BOOL NOT NULL DEFAULT false,
+--     PRIMARY KEY(id)
+-- );
+-- CREATE TABLE notification_person (
+--     notification_id BIGINT,
+--     administrator_person_id BIGINT,
+--     PRIMARY KEY(notification_id, administrator_person_id)
+-- );
+-- ALTER TABLE licitation
+-- ADD CONSTRAINT licitation_fk1 FOREIGN KEY (auction_id) REFERENCES auction(id);
+-- ALTER TABLE licitation
+-- ADD CONSTRAINT licitation_fk2 FOREIGN KEY (administrator_person_id) REFERENCES administrator(person_id);
+-- ALTER TABLE auction
+-- ADD CONSTRAINT auction_fk1 FOREIGN KEY (administrator_person_id) REFERENCES administrator(person_id);
+-- ALTER TABLE message
+-- ADD CONSTRAINT message_fk1 FOREIGN KEY (administrator_person_id) REFERENCES administrator(person_id);
+-- ALTER TABLE message
+-- ADD CONSTRAINT message_fk2 FOREIGN KEY (auction_id) REFERENCES auction(id);
+-- ALTER TABLE information
+-- ADD CONSTRAINT information_fk1 FOREIGN KEY (auction_id) REFERENCES auction(id);
+-- ALTER TABLE notification_person
+-- ADD CONSTRAINT notification_person_fk1 FOREIGN KEY (notification_id) REFERENCES notification(id);
+-- ALTER TABLE notification_person
+-- ADD CONSTRAINT notification_person_fk2 FOREIGN KEY (administrator_person_id) REFERENCES administrator(person_id);
+
+
+
+
+-- OUT WITH THE OLD, IN WITH THE NEW
+
+CREATE TABLE person (
+	id	 BIGSERIAL,
+	username VARCHAR(512) NOT NULL,
+	password VARCHAR(512) NOT NULL,
+	email	 VARCHAR(512) NOT NULL,
+	banned	 BOOL NOT NULL DEFAULT false,
+	PRIMARY KEY(id)
+);
+
 CREATE TABLE licitation (
-    price FLOAT(8) NOT NULL,
-    valid BOOL NOT NULL DEFAULT true,
-    auction_id BIGINT,
-    administrator_person_id BIGINT,
-    PRIMARY KEY(auction_id, administrator_person_id)
+	price	 FLOAT(8) NOT NULL,
+	valid	 BOOL NOT NULL DEFAULT true,
+	auction_id BIGINT,
+	person_id	 BIGINT,
+	PRIMARY KEY(auction_id,person_id)
 );
+
 CREATE TABLE auction (
-    id BIGSERIAL,
-    item BIGINT NOT NULL,
-    min_price FLOAT(8) NOT NULL DEFAULT 0,
-    end_date TIMESTAMP NOT NULL,
-    cancelled BOOL NOT NULL DEFAULT false,
-    administrator_person_id BIGINT NOT NULL,
-    PRIMARY KEY(id)
+	id	 BIGSERIAL,
+	item	 BIGINT NOT NULL,
+	min_price FLOAT(8) NOT NULL DEFAULT 0,
+	end_date	 TIMESTAMP NOT NULL,
+	cancelled BOOL NOT NULL DEFAULT false,
+	person_id BIGINT NOT NULL,
+	PRIMARY KEY(id)
 );
+
 CREATE TABLE users (
-    person_id BIGSERIAL,
-    person_username VARCHAR(512) NOT NULL UNIQUE,
-    person_password VARCHAR(512) NOT NULL,
-    person_email VARCHAR(512) NOT NULL,
-    person_banned BOOL NOT NULL DEFAULT false,
-    PRIMARY KEY(person_id)
+	person_id BIGINT,
+	PRIMARY KEY(person_id)
 );
+
 CREATE TABLE administrator (
-    person_id BIGSERIAL,
-    person_username VARCHAR(512) NOT NULL,
-    person_password VARCHAR(512) NOT NULL,
-    person_email VARCHAR(512) NOT NULL,
-    person_banned BOOL NOT NULL DEFAULT false,
-    PRIMARY KEY(person_id)
+	person_id BIGINT,
+	PRIMARY KEY(person_id)
 );
+
 CREATE TABLE message (
-    id BIGSERIAL,
-    content VARCHAR(1024) NOT NULL,
-    time_date TIMESTAMP NOT NULL,
-    administrator_person_id BIGINT NOT NULL,
-    auction_id BIGINT NOT NULL,
-    PRIMARY KEY(id)
+	id	BIGSERIAL,
+	content	 VARCHAR(512) NOT NULL,
+	time_date	 TIMESTAMP NOT NULL,
+	person_id	 BIGINT NOT NULL,
+	auction_id BIGINT NOT NULL,
+	PRIMARY KEY(id)
 );
+
 CREATE TABLE information (
-    reference BIGSERIAL,
-    title VARCHAR(512) NOT NULL,
-    description VARCHAR(1024) NOT NULL,
-    auction_id BIGINT NOT NULL,
-    PRIMARY KEY(reference)
+	reference	 BIGSERIAL,
+	title	 VARCHAR(512) NOT NULL,
+	description VARCHAR(512) NOT NULL,
+	auction_id	 BIGINT NOT NULL,
+	PRIMARY KEY(reference)
 );
+
 CREATE TABLE notification (
-    id BIGSERIAL,
-    content VARCHAR(1024) NOT NULL,
-    time_date TIMESTAMP NOT NULL,
-    was_read BOOL NOT NULL DEFAULT false,
-    PRIMARY KEY(id)
+	id	 BIGSERIAL,
+	content	 VARCHAR(512) NOT NULL,
+	time_date TIMESTAMP NOT NULL,
+	was_read	 BOOL NOT NULL DEFAULT false,
+	PRIMARY KEY(id)
 );
+
 CREATE TABLE notification_person (
-    notification_id BIGINT,
-    administrator_person_id BIGINT,
-    PRIMARY KEY(notification_id, administrator_person_id)
+	notification_id BIGINT,
+	person_id	 BIGINT,
+	PRIMARY KEY(notification_id,person_id)
 );
-ALTER TABLE licitation
-ADD CONSTRAINT licitation_fk1 FOREIGN KEY (auction_id) REFERENCES auction(id);
-ALTER TABLE licitation
-ADD CONSTRAINT licitation_fk2 FOREIGN KEY (administrator_person_id) REFERENCES administrator(person_id);
-ALTER TABLE auction
-ADD CONSTRAINT auction_fk1 FOREIGN KEY (administrator_person_id) REFERENCES administrator(person_id);
-ALTER TABLE message
-ADD CONSTRAINT message_fk1 FOREIGN KEY (administrator_person_id) REFERENCES administrator(person_id);
-ALTER TABLE message
-ADD CONSTRAINT message_fk2 FOREIGN KEY (auction_id) REFERENCES auction(id);
-ALTER TABLE information
-ADD CONSTRAINT information_fk1 FOREIGN KEY (auction_id) REFERENCES auction(id);
-ALTER TABLE notification_person
-ADD CONSTRAINT notification_person_fk1 FOREIGN KEY (notification_id) REFERENCES notification(id);
-ALTER TABLE notification_person
-ADD CONSTRAINT notification_person_fk2 FOREIGN KEY (administrator_person_id) REFERENCES administrator(person_id);
+
+ALTER TABLE licitation ADD CONSTRAINT licitation_fk1 FOREIGN KEY (auction_id) REFERENCES auction(id);
+ALTER TABLE licitation ADD CONSTRAINT licitation_fk2 FOREIGN KEY (person_id) REFERENCES person(id);
+ALTER TABLE auction ADD CONSTRAINT auction_fk1 FOREIGN KEY (person_id) REFERENCES person(id);
+ALTER TABLE users ADD CONSTRAINT user_fk1 FOREIGN KEY (person_id) REFERENCES person(id);
+ALTER TABLE administrator ADD CONSTRAINT administrator_fk1 FOREIGN KEY (person_id) REFERENCES person(id);
+ALTER TABLE message ADD CONSTRAINT message_fk1 FOREIGN KEY (person_id) REFERENCES person(id);
+ALTER TABLE message ADD CONSTRAINT message_fk2 FOREIGN KEY (auction_id) REFERENCES auction(id);
+ALTER TABLE information ADD CONSTRAINT information_fk1 FOREIGN KEY (auction_id) REFERENCES auction(id);
+ALTER TABLE notification_person ADD CONSTRAINT notification_person_fk1 FOREIGN KEY (notification_id) REFERENCES notification(id);
+ALTER TABLE notification_person ADD CONSTRAINT notification_person_fk2 FOREIGN KEY (person_id) REFERENCES person(id);
