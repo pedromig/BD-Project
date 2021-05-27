501d8bf0af63dbecf459352a742ae89d416afef5
Pedro Miguel Duque Rodrigues <pedror@student.dei.uc.pt>
2021-05-27 23:45:38 +0100

    mini merge

--- src/db/data.sql
+++ src/db/data.sql
@@ -1,63 +1,44 @@
 -- User --
-
-INSERT INTO person (
-    username,
-    password, 
-    email
-)
-VALUES 
-( -- id: 0
-    'admin',
-    'admin', 
-    'admin@admin.admin'
-),
-( -- id: 1
-    'Marega',
-    'Maregod', 
-    'marega@god.kekw'
-),
-( -- id: 2
-    'Pedro Mitalves',
-    'PoteDouro',
-    'seradesta@might.actually'
-);
-
-
+INSERT INTO person (username, password, email)
+VALUES (
+        -- id: 0
+        'admin',
+        'admin',
+        'admin@admin.admin'
+    ),
+    (
+        -- id: 1
+        'Marega',
+        'Maregod',
+        'marega@god.kekw'
+    ),
+    (
+        -- id: 2
+        'Pedro Mitalves',
+        'PoteDouro',
+        'seradesta@might.actually'
+    );
 -- Administrator --
-
 INSERT INTO administrator (person_id)
 VALUES (1);
-
 -- User --
-
 INSERT INTO users (person_id)
-VALUES (2), (3);
-
-
+VALUES (2),
+    (3);
 -- Auction --
-
 INSERT INTO auction (
-    item,
-    min_price,
-    end_date,
-    person_id
-)
-VALUES 
-(12344, 32.3, TIMESTAMP '2021-06-12 04:05:06', 1),
-(420, 4.20, TIMESTAMP '2021-04-20 04:20:00', 1),
-(123, 123.4, TIMESTAMP '2021-01-23 01:23:45', 2);
-
-
+        item,
+        min_price,
+        end_date,
+        person_id
+    )
+VALUES (12344, 32.3, TIMESTAMP '2021-06-12 04:05:06', 1),
+    (420, 4.20, TIMESTAMP '2021-04-20 04:20:00', 1),
+    (123, 123.4, TIMESTAMP '2021-01-23 01:23:45', 2);
 -- Information --
-
-INSERT INTO information (
-    title,
-    description,
-    auction_id
-)
-VALUES 
-(
-    'Lorem Ipsium', 
-    'Solumn Dolorem', 
-    1
-);
+INSERT INTO information (title, description, auction_id)
+VALUES (
+        'Lorem Ipsium',
+        'Solumn Dolorem',
+        1
+    );
